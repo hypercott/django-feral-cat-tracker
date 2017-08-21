2b4780fe6d7889fd5fa67e8746d20b1b6a0c0812
Christian D. Ott <christian.d.ott@gmail.com>
2017-08-20 19:01:28 -0700

    * add detailed view for Cat Colonies

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 
 from . import views
+from .models import CatColony
 
 urlpatterns = [
     url(r'^$', views.index, name='cattrack-index'),
-    url(r'^/colonies$', views.colonies, name='cattrack-colonies'),
+    url(r'^colonies$', views.colonies, name='cattrack-colonies'),
+#    url(r'^colonies/(?P<pk>[0-9]+)/$', views.DetailView.as_view(model=CatColony), name='cattrack-colony-detail'),
+#    url(r'^colonies/(?P<pk>[0-9]+)/$', views.ColonyDetail.as_view(), name='cattrack-colony-detail'),
+    url(r'^colonies/(?P<pk>[0-9]+)/$', views.colony_detail, name='cattrack-colony-detail'),
 ]
 
--- views.py
+++ views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.conf import settings
-from .models import CatColony
+from django.views.generic import DetailView
+from django.db.models import Q
+from .models import Cat,CatColony
 
 # Create your views here.
 def index(request):
@@ -18,10 +20,25 @@ def colonies(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect(settings.LOGIN_URL)
 
-    cols = CatColony.objects.all()
+    cols = CatColony.objects.filter(~Q(colony_name = "None"))
     
     #template = loader.get_template('cattrack/colonies.html')
     #context = {}
     #return HttpResponse(template.render(context,request))
     context = {'colonies_list': cols}
     return render(request, 'cattrack/colonies.html', context)
+
+#class ColonyDetail(DetailView):
+#    model = CatColony
+
+def colony_detail(request,pk):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(settings.LOGIN_URL)
+
+    col = CatColony.objects.get(pk=pk)
+    cats = Cat.objects.filter(cat_colony=pk)
+
+    context = {'catcolony' : col,
+               'cats' : cats}
+    return render(request, 'cattrack/catcolony_detail.html',context)
+
